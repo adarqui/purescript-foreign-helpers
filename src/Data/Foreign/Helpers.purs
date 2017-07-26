ae68e63b00c6218274f8e42f9ba3c135b200f89d
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-26 17:30:04 -0400

    working!

--- src/Data/Foreign/Helpers.purs
+++ src/Data/Foreign/Helpers.purs
@@ -8,7 +8,8 @@ module Data.Foreign.Helpers (
 
 
 import Control.Monad.Except.Trans (ExceptT, except)
-import Data.Argonaut.Decode.Class (decodeJson)
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson)
 import Data.Argonaut.Encode.Class (encodeJson)
 import Data.Either (Either(..), either)
 import Data.Foreign (Foreign, ForeignError(JSONError), fail, unsafeFromForeign)
@@ -16,16 +17,16 @@ import Data.Foreign.Index (readProp)
 import Data.List.Types (NonEmptyList(..))
 import Data.Identity (Identity)
 import Data.NonEmpty (singleton)
-import Prelude
+import Prelude (class Applicative, pure, (<$>), ($), (<<<))
 
 
 
--- readPropUnsafe :: forall t. String -> Foreign -> ExceptT (NonEmptyList ForeignError) Identity t
-readPropUnsafe :: forall t4. String -> Foreign -> ExceptT (NonEmptyList ForeignError) Identity t4
+readPropUnsafe :: forall a. String -> Foreign -> ExceptT (NonEmptyList ForeignError) Identity a
 readPropUnsafe x y = unsafeFromForeign <$> readProp x y
 
 
 
+decodeJsonRespondable :: forall a. DecodeJson a => Json -> Either (NonEmptyList ForeignError) a
 decodeJsonRespondable k =
   case decodeJson k of
        Left e -> Left $ NonEmptyList $ singleton $ JSONError e
@@ -33,9 +34,11 @@ decodeJsonRespondable k =
 
 
 
+exceptDecodeJsonRespondable :: forall a b. Applicative b => DecodeJson a => Json -> ExceptT (NonEmptyList ForeignError) b a
 exceptDecodeJsonRespondable = except <<< decodeJsonRespondable
 -- (except $ decodeJson x0)
 
 
 
+fromResponseDecodeJson :: forall a. DecodeJson a => Foreign -> ExceptT (NonEmptyList ForeignError) Identity a
 fromResponseDecodeJson json = either (\a -> fail $ JSONError "parse error") (\a -> pure a) (decodeJson (encodeJson (unsafeFromForeign json :: String)))
