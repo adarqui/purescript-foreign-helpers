fa52647800325939adaecfa2a9b964f773a54d76
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-26 17:27:52 -0400

    working!

--- src/Data/Foreign/Helpers.purs
+++ src/Data/Foreign/Helpers.purs
@@ -1,14 +1,41 @@
 module Data.Foreign.Helpers (
-  readPropUnsafe
+  readPropUnsafe,
+  decodeJsonRespondable,
+  exceptDecodeJsonRespondable,
+  fromResponseDecodeJson
 ) where
 
 
 
-import Prelude ((<$>))
-import Data.Foreign (Foreign(..), unsafeFromForeign)
+import Control.Monad.Except.Trans (ExceptT, except)
+import Data.Argonaut.Decode.Class (decodeJson)
+import Data.Argonaut.Encode.Class (encodeJson)
+import Data.Either (Either(..), either)
+import Data.Foreign (Foreign, ForeignError(JSONError), fail, unsafeFromForeign)
 import Data.Foreign.Index (readProp)
+import Data.List.Types (NonEmptyList(..))
+import Data.Identity (Identity)
+import Data.NonEmpty (singleton)
+import Prelude
 
 
 
 -- readPropUnsafe :: forall t. String -> Foreign -> ExceptT (NonEmptyList ForeignError) Identity t
+readPropUnsafe :: forall t4. String -> Foreign -> ExceptT (NonEmptyList ForeignError) Identity t4
 readPropUnsafe x y = unsafeFromForeign <$> readProp x y
+
+
+
+decodeJsonRespondable k =
+  case decodeJson k of
+       Left e -> Left $ NonEmptyList $ singleton $ JSONError e
+       Right v -> Right v
+
+
+
+exceptDecodeJsonRespondable = except <<< decodeJsonRespondable
+-- (except $ decodeJson x0)
+
+
+
+fromResponseDecodeJson json = either (\a -> fail $ JSONError "parse error") (\a -> pure a) (decodeJson (encodeJson (unsafeFromForeign json :: String)))
